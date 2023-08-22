c3dff718554722a1e9545bf76e359a5121871d6b
Paulo Lin <142857125+paulolinG@users.noreply.github.com>
2023-08-22 12:24:22 -0400

    Update test_standard_calc.cpp

--- c-cpp/test_standard_calc.cpp
+++ c-cpp/test_standard_calc.cpp
@@ -1,28 +1,84 @@
 #include "stdio.h"
-#include "cutest/CuTest.h"
+#include "cutest/CuTest.c"
+#include "standard_calc.cpp"
 #include "standard_calc.h"
 
 /**************** Tests for bound_to_180() *******************/
-void test_bound_basic1(CuTest *tc) {
-    float input = 180.0;
-    float actual = bound_to_180(input);
-    float expected = -180.0;
-    CuAssertDblEquals(tc, expected, actual, 0.0001);
+void test_bound_equalTo180(CuTest *tc) {
+    CuAssertDblEquals(tc, -180, bound_to_180(180), 0.0001);
+}
+
+void test_bound_greaterThan180(CuTest *tc) {
+    CuAssertDblEquals(tc, -159.445, bound_to_180(200.555), 0.0001);
+}
+
+void test_bound_greaterThan360(CuTest *tc) {
+    CuAssertDblEquals(tc, 20.555, bound_to_180(380.555), 0.0001);
+}
+
+void test_bound_greaterThan540(CuTest *tc) {
+    CuAssertDblEquals(tc, -159.445, bound_to_180(560.555), 0.0001);
+}
+
+void test_bound_inBoundaries(CuTest *tc) {
+    CuAssertDblEquals(tc, 0, bound_to_180(0), 0.0001);
+}
+
+void test_bound_equalToN180(CuTest *tc) {
+    CuAssertDblEquals(tc, -180, bound_to_180(-180), 0.0001);
+}
+
+void test_bound_lessThanN180(CuTest *tc) {
+    CuAssertDblEquals(tc, 139.6, bound_to_180(-220.4), 0.0001);
+}
+
+void test_bound_lessThanN360(CuTest *tc) {
+    CuAssertDblEquals(tc, -40.4, bound_to_180(-400.4), 0.0001);
+}
+
+void test_bound_lessThanN540(CuTest *tc) {
+    CuAssertDblEquals(tc, 139.6, bound_to_180(-580.4), 0.0001);
 }
 /**************** Tests for is_angle_between() *******************/
-void test_between_basic1(CuTest *tc) {
-    float firstAngle = 20.0;
-    float secondAngle = 30.0;
-    float thirdAngle = 40.0;
-    CuAssertTrue(tc, is_angle_between(firstAngle, secondAngle, thirdAngle));
+void test_between_bounding(CuTest *tc) {
+    CuAssertTrue(tc, is_angle_between(-580.4, 160, 180));
+}
+
+void test_between_firstGreaterSecond(CuTest *tc) {
+    CuAssertTrue(tc, is_angle_between(0, 20, 30));
+}
+
+void test_between_secondGreaterFirst(CuTest *tc) {
+    CuAssertTrue(tc, is_angle_between(30, 20, 0));
+}
+
+void test_between1(CuTest *tc) {
+    CuAssertTrue(tc, is_angle_between(-50, 10, 60));
+}
+
+void test_between2(CuTest *tc) {
+    CuAssertTrue(tc, is_angle_between(160, 170, -180));
 }
 
 int main(int argc, char const *argv[]) {
     CuString *output = CuStringNew();
     CuSuite *suite = CuSuiteNew();
 
-    SUITE_ADD_TEST(suite, test_bound_basic1);
-    SUITE_ADD_TEST(suite, test_between_basic1);
+    SUITE_ADD_TEST(suite, test_bound_equalTo180);
+    SUITE_ADD_TEST(suite, test_bound_greaterThan180);
+    SUITE_ADD_TEST(suite, test_bound_greaterThan360);
+    SUITE_ADD_TEST(suite, test_bound_greaterThan540);
+    SUITE_ADD_TEST(suite, test_bound_inBoundaries);
+    SUITE_ADD_TEST(suite, test_bound_equalToN180);
+    SUITE_ADD_TEST(suite, test_bound_lessThanN180);
+    SUITE_ADD_TEST(suite, test_bound_lessThanN360);
+    SUITE_ADD_TEST(suite, test_bound_lessThanN540);
+
+    SUITE_ADD_TEST(suite, test_between_bounding);
+    SUITE_ADD_TEST(suite, test_between_firstGreaterSecond);
+    SUITE_ADD_TEST(suite, test_between_secondGreaterFirst);
+    SUITE_ADD_TEST(suite, test_between1);
+    SUITE_ADD_TEST(suite, test_between2);
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);
